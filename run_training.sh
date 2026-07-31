7f21772f23fa8646c11470d24324be08bbbf1c51
Jan Sternberg <jan.sternberg@campus.lmu.de>
2026-07-31 10:01:22 +0200

    changed our run_training.sh to test on multiple scenarios

--- run_training.sh
+++ run_training.sh
@@ -19,49 +19,49 @@ if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
     LR="0.00001"
     EPOCHS="100"
     FLAGS="--balance_weights"
-    EXP_NAME="lr:0.00001 ep:100 30000DATA"
+    EXP_NAME="lr:0.00001 ep:100 30000DATA__balance_weights"
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
     LR="0.000001"
     EPOCHS="100"
     FLAGS="--balance_weights"
-    EXP_NAME="lr:0.000001 ep:100 30000DATA"
+    EXP_NAME="lr:0.000001 ep:100 30000DATA__balance_weights"
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
     LR="0.0000001"
     EPOCHS="100"
     FLAGS="--balance_weights"
-    EXP_NAME="lr:0.0000001 ep:100 30000DATA"
+    EXP_NAME="lr:0.0000001 ep:100 30000DATA__balance_weights"
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
     LR="0.000001"
     EPOCHS="200"
     FLAGS="--balance_weights"
-    EXP_NAME="lr:0.000001 ep:200 30000DATA"
+    EXP_NAME="lr:0.000001 ep:200 30000DATA__balance_weight"
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
     LR="0.000001"
     EPOCHS="1000"
-    FLAGS="--mirror --blur"
-    EXP_NAME="lr:0.000001 ep:1000 30000DATA_mirror_blurr"
+    FLAGS="--mirror --blur --balance_weights"
+    EXP_NAME="lr:0.000001 ep:1000 30000DATA_mirror_blurr_balance_weight"
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 6 ]; then
     LR="0.000001"
     EPOCHS="10000"
     FLAGS="--mirror --blur --cropmix --balance_weights" # <--- NEU: Hier testen wir alle 3 kombiniert!
-    EXP_NAME="lr:0.000001 ep:10000 30000DATA_mirror_blur_cropmix"
+    EXP_NAME="lr:0.000001 ep:10000 30000DATA_mirror_blur_cropmix_balance_weight"
 fi
 
 if [ $SLURM_ARRAY_TASK_ID -eq 7 ]; then
     LR="0.000005"
     EPOCHS="200"
-    FLAGS="--cropmix"                 # <--- NEU: Hier testen wir CropMix exklusiv!
-    EXP_NAME="lr:0.000005 ep:200 30000DATA_cropmix"
+    FLAGS="--cropmix --balance_weights"                 # <--- NEU: Hier testen wir CropMix exklusiv!
+    EXP_NAME="lr:0.000005 ep:200 30000DATA_cropmix_balance_weight"
 fi
 
 # 3. Den eigentlichen Befehl ausführen

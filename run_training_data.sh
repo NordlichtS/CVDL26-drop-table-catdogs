af509ace594b40282e0c97f8b0765d940a8578e8
Jan Sternberg <jan.sternberg@campus.lmu.de>
2026-07-31 10:02:42 +0200

    yeah pictures for our presentation needed to be created as well as for our final report

---
+++ run_training_data.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#SBATCH --job-name=catdogs_training         # Name des Jobs in der Queue
+#SBATCH --output=training_output_%A_%a.log  # %A = Gesamt-Job-ID, %a = Array-Index (1-7)
+#SBATCH --error=training_error_%A_%a.log    # Getrennte Fehler-Logs für jeden Run
+#SBATCH --partition=NvidiaAll               # Nutzt die Nvidia-Grafikkarten-Pools der LMU
+#SBATCH --array=1-7                         # Startet 7 Jobs parallel (Limit der LMU: max 15)
+#SBATCH --time=48:00:00                     # Erhöht auf 2 Tage, um Time-Limits abzufangen!
+
+# 1. In das richtige Projektverzeichnis wechseln
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+cd /home/st/sternberg/CVDL26-drop-table-catdogs
+
+source .venv/bin/activate
+
+# 2. Hyperparameter je nach Slurm-Array-ID zuweisen
+if [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
+    LR="0.00001"
+    EPOCHS="100"
+    FLAGS=""
+    EXP_NAME="lr:0.00001 ep:100 30000DATA_without_weights"
+fi
+
+if [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
+    LR="0.00001"
+    EPOCHS="100"
+    FLAGS="--balance_weights"
+    EXP_NAME="lr:0.00001 ep:100 30000DATA__balance_weights_without_weights"
+fi
+
+if [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
+    LR="0.00001"
+    EPOCHS="100"
+    FLAGS="--blur"
+    EXP_NAME="lr:0.00001 ep:100 30000DATA__blur_without_weights"
+fi
+
+if [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
+    LR="0.00001"
+    EPOCHS="100"
+    FLAGS="--mirror"
+    EXP_NAME="lr:0.00001 ep:100 30000DATA__mirror_without_weights"
+fi
+
+if [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
+    LR="0.00001"
+    EPOCHS="100"
+    FLAGS="--cropmix"
+    EXP_NAME="lr:0.00001 ep:100 30000DATA__cropmix_without_weights"
+fi
+
+if [ $SLURM_ARRAY_TASK_ID -eq 6 ]; then
+    LR="0.00001"
+    EPOCHS="100"
+    FLAGS="--cropmix --blur --mirror --balance_weights"
+    EXP_NAME="lr:0.00001 ep:100 30000DATA_all_without_weights"
+fi
+
+
+if [ $SLURM_ARRAY_TASK_ID -eq 7 ]; then
+    LR="0.000005"
+    EPOCHS="1000"
+    FLAGS=""
+    EXP_NAME="lr:0.000005 ep:1000 30000DATA_without_weights"
+fi
+
+# 3. Den eigentlichen Befehl ausführen
+python3 -u Final_Project/train.py \
+    --data_dir ./images \
+    --lr $LR \
+    --epochs $EPOCHS \
+    --exp_name "$EXP_NAME" \
+    $FLAGS
